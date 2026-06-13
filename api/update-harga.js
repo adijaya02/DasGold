d9ddf8c65214aebab7857b8f3ede48b72d1634cb
adijaya02 <pramana.putra0221@gmail.com>
2026-06-13 14:09:47 +0800

    Update update-harga.js

--- api/update-harga.js
+++ api/update-harga.js
@@ -1,5 +1,39 @@
-export default function handler(req, res) {
-  res.json({
-    hasKey: !!process.env.SUPABASE_KEY
-  });
+export default async function handler(req, res) {
+    try {
+        const response = await fetch(
+            "https://pegadaian.co.id/gold/prices/savings"
+        );
+
+        const data = await response.json();
+
+        const insert = await fetch(
+            "https://zasjkgrmcvigblpyqsff.supabase.co/rest/v1/harga_emas",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    apikey: process.env.SUPABASE_KEY,
+                    Authorization: `Bearer ${process.env.SUPABASE_KEY}`,
+                    Prefer: "return=representation"
+                },
+                body: JSON.stringify({
+                    tanggal: data.data.tglBerlaku,
+                    harga_beli: Number(data.data.hargaBeli),
+                    harga_jual: Number(data.data.hargaJual)
+                })
+            }
+        );
+
+        const result = await insert.text();
+
+        res.status(200).json({
+            status: insert.status,
+            result
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
 }
