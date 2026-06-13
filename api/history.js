1c21e38e4892385fa745ffc5239840a39db72803
adijaya02 <pramana.putra0221@gmail.com>
2026-06-13 16:17:00 +0800

    Update history.js

--- api/history.js
+++ api/history.js
@@ -1,7 +1,16 @@
 export default async function handler(req, res) {
     try {
+
+        const days = parseInt(req.query.days || "7");
+
+        const startDate = new Date();
+        startDate.setDate(startDate.getDate() - days);
+
+        const fromDate =
+            startDate.toISOString().split("T")[0];
+
         const response = await fetch(
-            "https://zasjkgrmcvigblpyqsff.supabase.co/rest/v1/harga_emas?select=tanggal,harga_beli,harga_jual&order=tanggal.asc",
+            `https://zasjkgrmcvigblpyqsff.supabase.co/rest/v1/harga_emas?select=tanggal,harga_beli,harga_jual&tanggal=gte.${fromDate}&order=tanggal.asc`,
             {
                 headers: {
                     apikey: process.env.SUPABASE_KEY,
