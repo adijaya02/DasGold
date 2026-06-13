d5ca8d435eac93550227ad4355a6335af47b862b
adijaya02 <pramana.putra0221@gmail.com>
2026-06-13 13:14:31 +0800

    Update update-harga.js

--- api/update-harga.js
+++ api/update-harga.js
@@ -1,27 +1,32 @@
 export default async function handler(req, res) {
-    const response = await fetch(
-        "https://pegadaian.co.id/gold/prices/savings"
-    );
+    try {
+        const response = await fetch(
+            "https://pegadaian.co.id/gold/prices/savings"
+        );
 
-    const data = await response.json();
+        const data = await response.json();
 
-    await fetch(
-        "https://zasjkgrmcvigblpyqsff.supabase.co/rest/v1/harga_emas",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                apikey: process.env.SUPABASE_KEY,
-                Authorization: `Bearer ${process.env.SUPABASE_KEY}`,
-                Prefer: "return=minimal"
-            },
-            body: JSON.stringify({
-                harga_beli: data.data.hargaBeli,
-                harga_jual: data.data.hargaJual,
-                tanggal: data.data.tglBerlaku
-            })
-        }
-    );
+        const result = await fetch(
+            "https://zasjkgrmcvigblpyqsff.supabase.co/rest/v1/harga_emas",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    apikey: process.env.SUPABASE_KEY,
+                    Authorization: `Bearer ${process.env.SUPABASE_KEY}`,
+                    Prefer: "return=minimal"
+                },
+                body: JSON.stringify({
+                    tanggal: data.data.tglBerlaku,
+                    harga_beli: Number(data.data.hargaBeli),
+                    harga_jual: Number(data.data.hargaJual)
+                })
+            }
+        );
 
-    res.json({ success: true });
+        res.json({ success: true });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 }
