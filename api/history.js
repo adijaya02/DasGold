96000762bae6befd8c2d560a04c110e3ea834894
adijaya02 <pramana.putra0221@gmail.com>
2026-06-14 16:29:13 +0800

    Update history.js

--- api/history.js
+++ api/history.js
@@ -1,14 +1,16 @@
 export default async function handler(req, res) {
     try {
-        const response = await fetch(
-            "https://zasjkgrmcvigblpyqsff.supabase.co/rest/v1/harga_emas?select=tanggal,harga_beli,harga_jual&order=tanggal.asc",
-            {
-                headers: {
-                    apikey: process.env.SUPABASE_KEY,
-                    Authorization: `Bearer ${process.env.SUPABASE_KEY}`
-                }
+
+        const url =
+            "https://zasjkgrmcvigblpyqsff.supabase.co/rest/v1/harga_emas?select=tanggal,harga_beli,harga_jual&order=tanggal.asc";
+
+        const response = await fetch(url, {
+            headers: {
+                apikey: process.env.SUPABASE_KEY,
+                Authorization: `Bearer ${process.env.SUPABASE_KEY}`,
+                "Content-Type": "application/json"
             }
-        );
+        });
 
         const data = await response.json();
 
