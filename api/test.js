cd8c51d304d29742bae344ef11da4d00456cd80f
adijaya02 <pramana.putra0221@gmail.com>
2026-06-14 16:21:58 +0800

    Delete api/test.js

--- api/test.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch(
-    "https://pegadaian.co.id/gold/prices/savings"
-  );
-
-  const data = await response.json();
-
-  res.status(200).json(data);
-}
