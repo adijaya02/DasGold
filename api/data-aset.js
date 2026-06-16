b31eeed37cb3b12e5d681eba6d8e6436bbe919db
adijaya02 <pramana.putra0221@gmail.com>
2026-06-16 20:19:33 +0800

    Update data-aset.js

--- api/data-aset.js
+++ api/data-aset.js
@@ -1,104 +1,47 @@
 export default async function handler(req, res) {
 
-    if (req.method !== "POST") {
-        return res.status(405).json({
-            error: "Method not allowed"
-        });
-    }
+try {
 
-    try {
+const url =
+"https://zasjkgrmcvigblpyqsff.supabase.co/rest/v1/data_aset?select=*&order=tanggal_aset.asc";
 
-        const {
-            tanggal_aset,
-            total_modal,
-            jumlah_uang_aplikasi,
-            untung_rugi
-        } = req.body;
+const response =
+await fetch(
+url,
+{
+headers:{
 
-        if (!tanggal_aset) {
-            return res.status(400).json({
-                error: "tanggal_aset wajib diisi"
-            });
-        }
+apikey:
+process.env.SUPABASE_KEY,
 
-        const response = await fetch(
-            "https://zasjkgrmcvigblpyqsff.supabase.co/rest/v1/data_aset",
-            {
-                method: "POST",
+Authorization:
+`Bearer ${process.env.SUPABASE_KEY}`,
 
-                headers: {
-                    apikey:
-                        process.env.SUPABASE_KEY,
+"Content-Type":
+"application/json"
 
-                    Authorization:
-                        `Bearer ${process.env.SUPABASE_KEY}`,
+}
+}
+);
 
-                    "Content-Type":
-                        "application/json",
+const data =
+await response.json();
 
-                    Prefer:
-                        "resolution=merge-duplicates"
-                },
+res
+.status(200)
+.json(data);
 
-                body: JSON.stringify({
+}
 
-                    tanggal_aset,
+catch(err){
 
-                    total_modal:
-                        Number(
-                            total_modal
-                        ) || 0,
+res
+.status(500)
+.json({
+error:
+err.message
+});
 
-                    jumlah_uang_aplikasi:
-                        Number(
-                            jumlah_uang_aplikasi
-                        ) || 0,
-
-                    untung_rugi:
-                        Number(
-                            untung_rugi
-                        ) || 0
-
-                })
-            }
-        );
-
-        const data =
-            await response.json();
-
-        if (!response.ok) {
-
-            return res.status(
-                response.status
-            ).json({
-                error: data
-            });
-
-        }
-
-        return res
-            .status(200)
-            .json({
-
-                success: true,
-
-                data
-
-            });
-
-    }
-
-    catch (error) {
-
-        return res
-            .status(500)
-            .json({
-
-                error:
-                    error.message
-
-            });
-
-    }
+}
 
 }
