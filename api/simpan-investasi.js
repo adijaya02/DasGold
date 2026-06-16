ed86746c79a3aafb74173b2aa3810b3060378e9b
adijaya02 <pramana.putra0221@gmail.com>
2026-06-16 20:31:34 +0800

    Update simpan-investasi.js

--- api/simpan-investasi.js
+++ api/simpan-investasi.js
@@ -1,86 +1,123 @@
 export default async function handler(req, res) {
 
-    if (req.method !== "POST") {
-        return res.status(405).json({
-            error: "Method not allowed"
-        });
-    }
+if (req.method !== "POST") {
+return res.status(405).json({
+error:
+"Method not allowed"
+});
+}
 
-    try {
+try {
 
-        const {
-            biaya_pembelian,
-            aset,
-            tanggal_beli
-        } = req.body;
+const {
 
-        if (
-            !biaya_pembelian ||
-            !aset ||
-            !tanggal_beli
-        ) {
-            return res.status(400).json({
-                error: "Data tidak lengkap"
-            });
-        }
+biaya_pembelian,
 
-        const response = await fetch(
-            "https://zasjkgrmcvigblpyqsff.supabase.co/rest/v1/investasi",
-            {
-                method: "POST",
-                headers: {
-                    apikey: process.env.SUPABASE_KEY,
-                    Authorization:
-                        `Bearer ${process.env.SUPABASE_KEY}`,
-                    "Content-Type": "application/json",
-                    Prefer: "return=representation"
-                },
+aset,
 
-                body: JSON.stringify([
-                    {
-                        biaya_pembelian,
-                        aset,
-                        tanggal_beli
-                    }
-                ])
-            }
-        );
+tanggal_beli
 
-       const data =
-        await response.json();
-        
-            console.log(
-            "STATUS:",
-            response.status
-        );
-        
-            console.log(
-            "SUPABASE:",
-            data
-        );
-        
-        if (!response.ok) {
-        
-            return res
-            .status(
-            response.status
-        )
-        .json({
-        
-            error:data
-        
-            });
-        
-        }
+}
+=
+req.body;
 
-        return res.status(200).json(data);
+if (
+!biaya_pembelian ||
+!aset ||
+!tanggal_beli
+) {
 
-    } catch (err) {
+return res
+.status(400)
+.json({
+error:
+"Data tidak lengkap"
+});
 
-        return res.status(500).json({
-            error: err.message
-        });
+}
 
-    }
+const response =
+await fetch(
+
+"https://zasjkgrmcvigblpyqsff.supabase.co/rest/v1/investasi",
+
+{
+
+method:
+"POST",
+
+headers:{
+
+apikey:
+process.env.SUPABASE_KEY,
+
+Authorization:
+`Bearer ${process.env.SUPABASE_KEY}`,
+
+"Content-Type":
+"application/json",
+
+Prefer:
+"resolution=merge-duplicates,return=representation"
+
+},
+
+body:
+JSON.stringify({
+
+tanggal_beli,
+
+biaya_pembelian,
+
+aset
+
+})
+
+}
+
+);
+
+const data =
+await response.json();
+
+if (
+!response.ok
+) {
+
+return res
+.status(
+response.status
+)
+.json(
+data
+);
+
+}
+
+return res
+.status(200)
+.json({
+
+success:
+true,
+
+data
+
+});
+
+}
+
+catch(err){
+
+return res
+.status(500)
+.json({
+
+error:
+err.message
+
+});
+
+}
 
 }
